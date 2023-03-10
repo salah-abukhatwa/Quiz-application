3a1df634db614050afa415abb6dd142779e42a07
salah-abukhatwa <salah.abukhatwa@gmail.com>
2023-03-10 21:17:18 +0300

    Add Data Function

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 // Sellect Elements
 let countSpan = document.querySelector(".quiz-info .count span");
 let bulletsSpanContainer = document.querySelector(".bullets .spans");
-
+let quizArea = document.querySelector(".quiz-area");
+let answersArea = document.querySelector(".answers-area");
+//   Set Options
+let currentIndex = 0;
 
 
 function getQuestions() {
@@ -12,11 +15,14 @@ function getQuestions() {
         if (this.readyState === 4 && this.status === 200) {
             let questionsObject = JSON.parse(this.responseText);
 
-            let questionsCount = questionsObject.length
-            console.log(questionsCount);
+            let qCount = questionsObject.length
+            console.log(qCount);
 
             // Creat Bullets + Set Questions Count
-            createBullets(questionsCount);
+            createBullets(qCount);
+
+            // Add Question Data
+            addQuestionData(questionsObject[currentIndex], qCount);
         }
     };
 
@@ -42,4 +48,47 @@ function createBullets(num) {
         }
         
     }
+}
+
+function addQuestionData(obj , count) {
+// Creat H2 Question Title
+    let questionTitle = document.createElement("h2");
+    // Creat Question Text
+    let questionText = document.createTextNode(obj.title);
+    questionTitle.appendChild(questionText);
+    quizArea.appendChild(questionTitle);
+
+    // Creat Answers
+    for (let i = 1; i <= 4; i++) {
+        // Creat Main Answers
+    let mainDiv = document.createElement("div");
+        mainDiv.className = "answer";
+        let radioInput = document.createElement("input");
+        // Add Type + Name + Id + Data-Attribute
+        radioInput.name = 'question';
+        radioInput.type = 'radio';
+        radioInput.id = `answer_${i}`;
+        radioInput.dataset.answer = obj[`answer_${i}`];
+
+        // Make First Option Selected
+        if (i === 1) {
+            radioInput.checked = true;
+        }
+
+        // Create Label
+        let theLabel = document.createElement("label");
+
+        // Add For Attribute
+        theLabel.htmlFor = `answer_${i}`;
+        let theLabelText = document.createTextNode(obj[`answer_${i}`]);
+        theLabel.appendChild(theLabelText);
+
+        // Add Input + Label To Main Div
+        mainDiv.appendChild(radioInput);
+        mainDiv.appendChild(theLabel);
+        answersArea.appendChild(mainDiv);
+
+        
+    }
+    
 }
